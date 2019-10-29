3e800af7b5866dfdc5b93c375d2d4d88160b6f9d
esko <esko@pop-os.localdomain>
2019-10-29 15:40:47 +0200

    proofs and stuff

--- Week1.hs
+++ Week1.hs
@@ -1,5 +1,7 @@
 {-# Language FlexibleInstances #-}
-module Week1where
+{-# Language FlexibleContexts #-}
+{-# GHC_OPTIONS -fprint-potential-instances #-}
+module Week1 where
 
 import Prelude hiding (Eq, (==))
 import Data.List.NonEmpty 
@@ -9,29 +11,78 @@ import Data.Map as Map
 class Eq a where
   (==) :: a -> a -> Bool
 
--- Bools are a finite set of predetermined values,
--- so you can check states against each other
+
 instance Eq Bool where
   (==) True True   = True
   (==) False False = True
   (==) _     _     = False
+{-
 
--- 
+(~):
+  Has the corresponding type.
+
+r:
+  First two rules go trough all possible states of reflexivity for boolean values.
+
+s:
+  In the first two rules there is no distinction between the second and the first element.
+
+t:
+  The first and the second part of the conjunction ( 1.(x~y)=1, 2.(y~z)=1 ) can only apply if the arguments are both True or if they are both False. As such the operation has to evaluate to truth among any of the defined elements as they are all the same.
+
+-}
+
+ 
 instance Eq a => Eq (Maybe a) where
   (==) (Just x) (Just y) = x == y
   (==) Nothing Nothing   = True
   (==) _       _         = False
+
+{-
+
+(~):
+  Has the corresponding type.
+
+r:
+  In Maybe there are two possible top level warpper types:
+  Nothing: in which case reflexive attribute follows directly from the definition
+  (Just _): where the reflexivity applies if the reflexivity applies to the values contained within in the Just.
+
+s:
+  There is nothing separating the arguments from each other in relation to their order, outside of the (==) operator, and we assume that it follows symmetric rule.
+
+t:
+  In case of Nothing:
+    The first and the second part of the conjunction ( 1.(x~y)=1, 2.(y~z)=1 ) can only apply if the arguments are both Nothing. As such the operation has to evaluate to truth among any of the defined elements as they are all the same.
+  In case of Just _:
+    The rule applies if it applies for other instances of (==) for the value inside the Just. As we can just replace the Just values with the equality of the values contained within. 
+
+-}
+
 
 instance (Eq a, Eq b) => Eq (Either a b) where
   (==) (Left x) (Left y)   = x == y
   (==) (Right x) (Right y) = x == y
   (==) _         _         = False
 
+
+
+
 instance (Eq a, Eq b) => Eq (a, b) where
   (==) (x1, x2) (y1, y2) = (x1 == y1) && (x2 == y2)
 
 instance Eq a => Eq (a -> a) where
   (==) _ _ = True
+
+{-
+
+(~): The equality funtion is defined with the correct type.
+r: Neither of the inputs are related to the output so the rule holds
+s: Neither of the inputs are related to the output so the rule holds
+t: Neither of the inputs are related to the output so the rule holds
+
+-}
+  
 
 instance Eq a => Eq [a] where
   --(==) []  []       = True
@@ -51,8 +102,4 @@ instance Eq Void where
 instance (Ord k, Eq a) => Eq (Map k a) where
   (==) xs ys = isSubmapOfBy (==) xs ys && isSubmapOfBy (==) ys xs  
 
-
-fun :: [a] -> [a] -> Bool
-fun []      ys    = False
-fun []      []    = True
-fun [x] (y:ys)    = True 
+--tests = (id::Eq (a->a) => (a->a)) == (id::Eq (a->a) => (a->a))
