f7bc8cff7a140ba416286f87049f58eec212eb2e
Oskari Alanko <oskari.alanko@gmail.com>
2019-11-04 15:03:22 +0200

    waka waka

--- Week2_ex1.hs
+++ Week2_ex1.hs
@@ -2,10 +2,13 @@ module Week2_ex1 where
 
 import Prelude hiding (Functor, fmap)
 import Data.List.NonEmpty
+import Data.Monoid.Endo
 
 class Functor m where
   fmap :: (a -> b) -> m a -> m b
 
+
+-- Bool doesn't admit a functor, because it's not a wrapper of any kind
 
 instance Functor Maybe where
   fmap f (Just x) = Just (f x)
@@ -17,6 +20,10 @@ instance Functor (Either a) where
 
 instance Functor ((,) a) where
   fmap f (x, y) = (x, f y)
+
+-- Endo a contains function of type a -> a and in fmap we have no function to convert the input value of the function into the end type endo b where the function would be of type b -> b as we only have a function of type (a -> b) in fmap.
+-- instance Functor Endo where
+--   fmap f (Endo g) = Endo ()
 
 instance Functor [] where
   fmap _ []     = []
