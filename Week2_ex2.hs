49cccca15e3bcc4bdc71c9a526ce93f2f7fcdb56
Oskari Alanko <oskari.alanko@gmail.com>
2019-11-05 14:47:51 +0200

    more ex2

--- Week2_ex2.hs
+++ Week2_ex2.hs
@@ -9,26 +9,63 @@ import Data.Functor.Compose
 import Data.Functor.Const
 import Data.Proxy
 import Data.Sequence.Internal
+import Control.Dsl.Cont
+import Data.Profunctor
+import Data.Functor.Yoneda
+import Data.Functor.Coyoneda
+
 
 class Functor m where
   fmap :: (a -> b) -> m a -> m b
 
 
-instance Functor (Sum f g) where
-  fmap ff (InL x) = undefined
-  fmap ff (InR x) = undefined
+instance (Functor f, Functor g) => Functor (Sum f g) where
+  fmap ff (InL x) = InL (fmap ff x)
+  fmap ff (InR x) = InR (fmap ff x)
 
-instance Functor (Product m n) where
-  fmap ff (Pair xs ys) = undefined 
+instance (Functor m, Functor n) => Functor (Product m n) where
+  fmap ff (Pair xs ys) = (Pair (fmap ff xs) (fmap ff ys))
 
 instance Functor Identity where
   fmap f (Identity x) = Identity (f x)
 
-instance Functor (Compose m n) where
-  fmap f (Compose xs) = undefined
+instance (Functor m, Functor n) => Functor (Compose m n) where
+  fmap f (Compose xs) = (Compose (fmap f' xs)) where
+    f' x = fmap f x  
 
-instance Functor (Const b) where
-  fmap f (Const x) = undefined
+instance Functor (Const a) where
+  fmap f (Const x) = Const x
 
--- Proxy can't be meaningfully implemented, because it can't hold data
+instance Functor Proxy where
+  fmap f xs = Proxy
 
+instance Functor (State s) where
+  fmap f (State xs) = State (g . xs)
+    where
+      g (a, b) = (a, f b)
+
+-- f :: b -> c
+-- e :: (c -> a) -> (b -> a)
+-- e ca = ca . f
+
+-- r . e = g
+-- r :: (b -> a) -> a
+-- g :: (c -> a) -> a
+instance Functor (Cont a) where
+  fmap f (Cont xs) = Cont (xs . e)
+    where
+      e ca = ca . f
+
+instance Functor m => Functor (Star m a) where
+  fmap f (Star xs) = Star(g . xs)
+    where
+      g = fmap f
+
+instance Functor (Costar m a) where
+  fmap f (Costar g) = Costar (f . g)
+
+instance Functor m => Functor (Yoneda m) where
+  fmap f (Yoneda xs) = undefined
+
+instance Functor (Coyoneda m) where
+  fmap f (Coyoneda _ _) = undefined
