3b56044afe751354edbf25e1790ba593de4d5695
Oskari Alanko <oskari.alanko@gmail.com>
2019-11-18 17:33:21 +0200

    nonempty

--- src/Week4/Exercise1.hs
+++ src/Week4/Exercise1.hs
@@ -1,26 +1,34 @@
 module Week4.Exercise1 where
 import Prelude hiding (Monad, return, (>>=))
+import Data.List.NonEmpty as Esko
 
 
-class Applicative  m=> Monad m where  
-    return :: a -> m a  
-    (>>=) :: m a -> (a -> m b) -> m b  
+class Applicative m => Monad m where  
+    return :: a -> m a
+    return = pure
+    (>>=) :: m a -> (a -> m b) -> m b
+    (>>)  :: m a -> m b -> m b
+    m >> n = m >>= \_ -> n
 --------------------------------------------------------------------------------
 
 -- Bool is not a Monad
 
 --------------------------------------------------------------------------------
 instance Monad Maybe where
-    return x = Just x  
     Nothing >>= f = Nothing  
     Just x >>= f  = f x  
- --------------------------------------------------------------------------------
-instance Monad [] where  
-    return x = [x]  
-    xs >>= f = concat (map f xs)  
- --------------------------------------------------------------------------------
- 
+--------------------------------------------------------------------------------
+instance Monad [] where
+    xs >>= f = concat (Prelude.map f xs)  
+--------------------------------------------------------------------------------
+instance Monad NonEmpty where
+  (x :| xs) >>= f = y :| ys ++ zs
+    where
+      (y :| ys) = f x
+      zs = xs >>= (toList . f)
+      toList (c :| cs) = c:cs
+--------------------------------------------------------------------------------
 instance Monoid a => Monad ((,) a) where
-    return x = (mempty,x)
     (x,y) >>= f = (x, snd (f y))
 --------------------------------------------------------------------------------
+
