ef52fb51b7bd4d05cd24ddda51b36044847a6f8e
Oskari Alanko <oskari.alanko@gmail.com>
2019-11-18 13:04:53 +0200

    parser stuff

--- src/Week3/Exercise4.hs
+++ src/Week3/Exercise4.hs
@@ -1,14 +1,20 @@
 module Week3.Exercise4 where
 
+import Control.Applicative
 import Week3.Exercise3
 
+ 
+record = (\x -> \y -> x:y) <$> unit <*> recordTail
+recordTail = (many ((flip const) <$> unitSeparator <*> unit))
+recordSeparator = single '\n' 
 
-unitValue = (\x -> [x]) <$> noneOf [',']
+unit = unitValueUnquoted <|> unitValueQuoted
+unitValueUnquoted = some (noneOf [',', '\n', '\r', '"'])
+unitValueQuoted = flip const <$> single '"'
+                  <*> (const <$> some (anySingleBut '"')
+                       <*> single '"')
+--doesn't work
+insideQuotes = fmap concat (some ((chunk "\"\"") <|> fmap (\x -> [x]) (anySingleBut '"')))
 
-separator = single ','
+unitSeparator = single ','
 
-
-
--- chunk :: String -> Parser String
--- chunk []     = fmap (\x -> "") eof
--- chunk (x:xs) = pure (++) <*> (fmap (\x -> [x]) (single x)) <*> chunk xs
