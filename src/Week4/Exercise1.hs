dedea987bfd1ae1d6927972a47774a6734a5e423
NaelSk <nael.soukouti@gmail.com>
2019-11-18 15:46:21 +0200

    May as Monads is implemented

--- src/Week4/Exercise1.hs
+++ src/Week4/Exercise1.hs
@@ -1,1 +1,16 @@
 module Week4.Exercise1 where
+
+
+class Applicaative => Monad m where  
+    return :: a -> m a  
+    (>>=) :: m a -> (a -> m b) -> m b  
+--------------------------------------------------------------------------------
+
+-- Bool is not a Monad
+
+--------------------------------------------------------------------------------
+instance Monad Maybe where
+    return x = Just x  
+    Nothing >>= f = Nothing  
+    Just x >>= f  = f x  
+ --------------------------------------------------------------------------------
