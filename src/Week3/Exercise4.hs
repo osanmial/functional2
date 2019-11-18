a789dfd9d82bd98e885bb2e6189ce35e0356f314
Oskari Alanko <oskari.alanko@gmail.com>
2019-11-18 15:08:07 +0200

    ex4 done

--- src/Week3/Exercise4.hs
+++ src/Week3/Exercise4.hs
@@ -3,17 +3,31 @@ module Week3.Exercise4 where
 import Control.Applicative
 import Week3.Exercise3
 
- 
-record = (\x -> \y -> x:y) <$> unit <*> recordTail
-recordTail = (many ((flip const) <$> unitSeparator <*> unit))
-recordSeparator = single '\n' 
+group = (:) <$> record <*> ((++) <$> groupTail <*> (fmap (\x -> []) eof))
+groupTail = many ((flip const) <$> recordSeparator <*> record)
 
-unit = unitValueUnquoted <|> unitValueQuoted
+record = (:) <$> unit <*> recordTail
+recordTail = (many ((flip const) <$> unitSeparator <*> unit))
+recordSeparator = fmap (\x -> [x]) (single '\n') <|> chunk2 "\r\n"
+
+unit = unitValueUnquoted <|> unitValueQuoted <|> pure ""
 unitValueUnquoted = some (noneOf [',', '\n', '\r', '"'])
 unitValueQuoted = flip const <$> single '"'
                   <*> (const <$> insideQuotes
                        <*> single '"')
 insideQuotes = fmap concat (some ((chunk2 "\"\"") <|> fmap (\x -> [x]) (anySingleBut '"')))
-
 unitSeparator = single ','
 
+
+testcase = "#0,#1,#2,#3,#4,#5,#6,#7,#8,#9\n\
+           \0,1,2,3,4,5,6,7,8\n\
+           \1,1,1,1,one as a string,1,1,1,1\n\
+           \2,1,2,1,2,1,2,1,2\n\
+           \3,1,1,3,\"one, but with a comma\",1,3,1,1\n\
+           \4,1,2,1,4,1,2,1,4\n\
+           \5,1,1,1,\"one accompanied by\n\
+           \a line break\",5,1,1,1\n\
+           \6,1,2,3,2,1,6,1,2\n\
+           \7,1,1,1,\"one involving \"\"quotation\"\" marks\",1,1,7,1\n\
+           \8,1,2,1,4,1,2,1\n\
+           \,,,,\"one among missing values\",,,,,9"
