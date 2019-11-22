fde138f3db609ad2d1bb724ebc6686340e8f0412
Oskari Alanko <oskari.alanko@gmail.com>
2019-11-22 15:17:59 +0200

    cleanup

--- src/Week4/Exercise4.hs
+++ src/Week4/Exercise4.hs
@@ -36,29 +36,78 @@ instance Monad Parser where
 pExpr :: Parser Expr
 pExpr = pLeft <|> pLet <|> pSub <|> pVar --pOne <|> pZero
 
+----Primitive 'number' parsers--------------------------------------------------
+
+pZero :: Parser Expr
+pZero = do
+  single '0'
+  pure Zero
+  
+pOne :: Parser Expr
+pOne = do
+  single '1'
+  pure One
+
+pNum = pOne <|> pZero
+
+--------------------------------------------------------------------------------
+----Character parsers-----------------------------------------------------------
+
+pSmall :: Parser Char
+pSmall = oneOf ['a'..'z'] -- [a-z] | [_] ;
+pLarge :: Parser Char
+pLarge = oneOf ['A'..'Z'] -- [A-Z] ;
+pDigit :: Parser Char
+pDigit = oneOf ['0'..'9'] -- [0-9] ;
+pPrime :: Parser Char
+pPrime = single '\'' -- ['] ;
+
+removeEmpties :: Parser String
+removeEmpties = (do
+  e1 <- oneOf ['\t','\n','\f','\r', ' ']
+  e2 <- removeEmpties
+  (pure "")) <|> (:[]) <$> (oneOf ['\t','\n','\f','\r']) <|> (pure "")
+  -- have no clue what is a "line tabulation" or how to deal with it: '\u000b'
+  -- [\t\n\u000b\f\r ] + -> skip ;
+
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+-- Parses beginning of an expression
+-- 1 ...
 pLeft = (do
   n <- pNum
   c <- pCalc n
   (pure c)) <|> pNum
   -- (pure pMul One) <|> (end)
-pNum = pOne <|> pZero
 
+-- Parses the tail of an expression
+-- General version
+-- ... + 1 * 1
 pCalc :: Expr -> Parser Expr
 pCalc left = pMul left <|> pAdd left
 
+-- Parses the tail of an expression
+-- In the case that it's a plus 
+pAdd :: Expr -> Parser Expr
 pAdd left  = do
   removeEmpties
-  term          <- single '+'
+  single '+'
   removeEmpties
   right         <- pLeft 
   (return $ Add left right)
 
+-- Parses the tail of an expression
+-- In the case that it's a multiplication
+pMul :: Expr -> Parser Expr
 pMul left = do
-  space <- removeEmpties
-  term <- single '*'
-  space <- removeEmpties
+  removeEmpties
+  single '*'
+  removeEmpties
   right <- pOther
   (pure $ Mul left right)
+
 
 pOther :: Parser Expr
 pOther =  pSub <|> pZero <|> pOne <|> pLet <|> pVar
@@ -70,17 +119,7 @@ pSub = do
   ex <- pExpr
   single ')'
   pure ex
-  
-pZero :: Parser Expr
-pZero = do
-  single '0'
-  pure Zero
-  
-pOne :: Parser Expr
-pOne = do
-  single '1'
-  pure One
-
+ 
 
 pLet :: Parser Expr  
 pLet = do
@@ -96,6 +135,8 @@ pLet = do
   chunk2 "in"
   removeEmpties
   expr2         <- pExpr
+  -- /\ this part fails in situations like "let abc = 1 in 1 + 1 * 1"
+  -- it doesn't parse past multiplication
   removeEmpties
   pure $ Let var expr1 expr2
 
@@ -114,26 +155,12 @@ pIdent' :: Parser String
 pIdent' = (do
   a1 <- oneFromLiftedParserList
   a2 <- pIdent'
-  pure (a1 ++ a2)) <|> oneFromLiftedParserList --replace ++ with something more effective. It happens backwards here I think.
+  pure (a1 ++ a2)) <|> oneFromLiftedParserList
+  --replace ++ with something more effective. It happens backwards here I think.
 
 oneFromLiftedParserList :: Parser [Char]
 oneFromLiftedParserList = foldl1' (<|>) $ (fmap (:[])) <$> [pSmall, pLarge, pDigit, pPrime]
   
-pSmall :: Parser Char
-pSmall = oneOf ['a'..'z'] -- [a-z] | [_] ;
-pLarge :: Parser Char
-pLarge =oneOf ['A'..'Z'] -- [A-Z] ;
-pDigit :: Parser Char
-pDigit =oneOf ['0'..'9'] -- [0-9] ;
-pPrime :: Parser Char
-pPrime =single '\'' -- ['] ;
-
-removeEmpties :: Parser String
-removeEmpties = (do
-  e1 <- oneOf ['\t','\n','\f','\r', ' ']
-  e2 <- removeEmpties
-  (pure "")) <|> (:[])<$> (oneOf ['\t','\n','\f','\r']) <|> (pure "") -- have no clue what is a "line tabulation" or how to deal with it: '\u000b'
-  -- [\t\n\u000b\f\r ] + -> skip ;
 
 
 
