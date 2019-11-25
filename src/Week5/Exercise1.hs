db05cff207d8c9ab9662c4ca1abf4847323564a1
Oskari Alanko <oskari.alanko@gmail.com>
2019-11-25 14:56:39 +0200

    foldable class

--- src/Week5/Exercise1.hs
+++ src/Week5/Exercise1.hs
@@ -5,3 +5,8 @@ import Prelude hiding (Monad, return, (>>=), (>>))
 import Data.List.NonEmpty as Esko hiding (map)
 
 import GHC.Base hiding (Monad, (>>=), (>>))
+
+
+class Foldable t where
+  foldMap :: Monoid m => (a -> m) -> t a -> m
+  foldr :: (a -> b -> b) -> b -> t a -> b 
