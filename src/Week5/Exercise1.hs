c65d56afc57c81a40ab340f86342b49db3a902b5
NaelSk <nael.soukouti@gmail.com>
2019-11-25 14:05:10 +0200

    But the basestone week5

---
+++ src/Week5/Exercise1.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE MagicHash, UnboxedTuples #-}
+module Week5.Exercise1 where
+import Prelude hiding (Monad, return, (>>=), (>>))
+import Data.List.NonEmpty as Esko hiding (map)
+
+import GHC.Base hiding (Monad, (>>=), (>>))
