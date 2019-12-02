b9bdb5f0c440012b0dfc501782749a3f949c83b0
esko <esko@pop-os.localdomain>
2019-12-02 17:49:34 +0200

    Ex1 something

--- src/Week6/Exercise1.hs
+++ src/Week6/Exercise1.hs
@@ -7,11 +7,21 @@ import Data.Functor.Identity
 newtype WrappedTraversable m a = WrapTraversable {unwrapTraversable :: m a}
 
 instance Traversable m => Functor (WrappedTraversable m) where
-  fmap f (WrapTraversable xs) = WrapTraversable (fmap f xs)
+  fmap f (WrapTraversable xs) = WrapTraversable (traverse g xs) where
+    g =  . f 
+
+-- f :: a -> b
+-- traverse :: (Traversable t, Applicative f) => (a -> f b) -> t a -> f (t b)
+
+  
 
 instance Traversable m => Foldable (WrappedTraversable m) where
   foldMap f (WrapTraversable xs) = foldMap f xs
 
 instance Traversable m => Traversable (WrappedTraversable m) where
   sequenceA = traverse id
-  traverse k (WrapTraversable xs) =  WrapTraversable <$> traverse  k xs
+  traverse k (WrapTraversable xs) =  WrapTraversable <$> traverse k xs
+
+
+
+
