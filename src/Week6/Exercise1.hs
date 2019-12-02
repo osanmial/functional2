3a1a1650c6be4e7c9552c7961eeaead11beb876c
NaelSk <nael.soukouti@gmail.com>
2019-12-02 14:15:25 +0200

    Start EX1

--- src/Week6/Exercise1.hs
+++ src/Week6/Exercise1.hs
@@ -1,1 +1,17 @@
 module Week6.Exercise1 where
+
+import Data.Functor.Compose
+import Data.Functor.Const
+import Data.Functor.Identity
+
+newtype WrappedTraversable m a = WrapTraversable {unwrapTraversable :: m a}
+
+instance Traversable m => Functor (WrappedTraversable m) where
+  fmap f (WrapTraversable xs) = undefined
+
+instance Traversable m => Foldable (WrappedTraversable m) where
+  foldMap f (WrapTraversable xs) = undefined
+
+instance Traversable m => Traversable (WrappedTraversable m) where
+  sequenceA (WrapTraversable xs) = undefined
+  traverse k (WrapTraversable xs) = undefined
