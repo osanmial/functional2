08a7d253609a3fc6d60f0212853e4e83e7843994
NaelSk <nael.soukouti@gmail.com>
2019-12-03 14:15:37 +0200

    Clear vision

--- src/Week6/Exercise1.hs
+++ src/Week6/Exercise1.hs
@@ -5,22 +5,28 @@ import Data.Functor.Const
 import Data.Functor.Identity
 
 newtype WrappedTraversable m a = WrapTraversable {unwrapTraversable :: m a}
--- traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
 instance Traversable m => Functor (WrappedTraversable m) where
+  --fmap f  (WrapTraversable xs)=  WrapTraversable $ (runIdentity.traverse (Identity . f)) xs
+    fmap f  = runIdentity.traverse (Identity . f) 
+  
+  -- traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
   -- runIdentity :: Identity a -> a
   -- identity :: a -> identity a
   -- f :: a -> b 
   -- Identity:: a -> identity b
-  --fmap f (WrapTraversable xs) =  WrapTraversable $  runIdentity.traverse (Identity . f) <$> xs
-  fmap f =  runIdentity.traverse (Identity . f) 
+  
   -- this also works but why the above doesn't need to unwrap? Because newtype I guess?
   -- fmap f (WrapTraversable xs) =  WrapTraversable . runIdentity . traverse (Identity . f) $ xs
 
 instance Traversable m => Foldable (WrappedTraversable m) where
+   -- traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
+   -- The below is working:
+   --foldMap f  (WrapTraversable xs)= WrapTraversable $ (getConst . traverse (Const . f) ) xs
+     foldMap f  = getConst . traverse (Const . f) 
   -- Const :: a -> Const a b
   -- getConst :: Const a b -> a
   -- f :: a -> m 
-  foldMap f  = getConst . traverse (Const . f)
+  
 
 instance Traversable m => Traversable (WrappedTraversable m) where
   sequenceA = traverse id
