0f3a6acea9e9b258c27b847d56b394a6c53bcfdc
Oskari Alanko <oskari.alanko@gmail.com>
2019-12-09 16:21:51 +0200

    wakawaka

--- src/Week7/Exercise1.hs
+++ src/Week7/Exercise1.hs
@@ -2,3 +2,16 @@ module Week7.Exercise1 where
 
 fix :: (a -> a) -> a
 fix f = f (fix f)
+
+id' = fix (const id)
+
+repeat' :: a -> [a]
+repeat' x = fix (x:)
+
+reverse' :: [a] -> [a]
+reverse' = fix f
+  where
+    f r (x:xs) = (r xs) ++ [x]
+    f _ []     = []
+
+fix' = fix (const fix)
