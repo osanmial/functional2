45145459b6776fcebb809bce2b91ab762892c16f
esko <esko@pop-os.localdomain>
2019-12-09 16:18:36 +0200

    ++ doen

--- src/Week7/Exercise1.hs
+++ src/Week7/Exercise1.hs
@@ -1,4 +1,23 @@
+{-# OPTIONS_GHC -fwarn-incomplete-patterns  #-}
+{-# OPTIONS_GHC -fwarn-hi-shadowing #-}
+{-# OPTIONS_GHC -fwarn-name-shadowing #-}
 module Week7.Exercise1 where
 
 fix :: (a -> a) -> a
 fix f = f (fix f)
+
+id' :: a -> a
+id' = fix f where
+  f b a = a
+
+(++:) :: [a] -> [a] -> [a]
+(++:) as bs = fix (++::) as bs
+
+(++::) :: ([a] -> [a]-> [a]) -> [a] -> [a] -> [a]
+(++::) r [] bs = bs
+(++::) r (a:as) bs = a : r as bs
+
+(++**) :: a -> [a] -> [a]
+(++**) r a = undefined
+
+
