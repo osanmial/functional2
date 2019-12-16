77303da184cbff5745ff33e806f28668da38456c
NaelSk <nael.soukouti@gmail.com>
2019-12-16 15:24:12 +0200

    isSimple EX3 Week 7

--- src/Week7/Exercise3.hs
+++ src/Week7/Exercise3.hs
@@ -1,1 +1,19 @@
 module Week7.Exercise3 where
+import Control.Exception
+import Data.Map
+import System.IO.Unsafe (unsafePerformIO)
+    
+
+
+
+data Expr = Add Expr Expr | Zero | Mul Expr Expr | One |
+    Let String Expr Expr | Var String
+    deriving Show
+
+isSimple :: Expr -> Bool
+isSimple exp= case exp of 
+    Let _ _ _ -> False
+    otherwise-> case exp of 
+        Var _ ->False
+        otherwise->  True
+  
