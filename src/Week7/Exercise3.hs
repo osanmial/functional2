5f4330e6ea244b0157581d703b4afc204235b13a
NaelSk <nael.soukouti@gmail.com>
2019-12-16 16:42:30 +0200

    Add import stuff

--- src/Week7/Exercise3.hs
+++ src/Week7/Exercise3.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Week7.Exercise3 where
-import Control.Exception
-import Data.Map
-import System.IO.Unsafe (unsafePerformIO)
-    
-data Expr = Add Expr Expr | Zero | Mul Expr Expr | One |
-    Let String Expr Expr | Var String
-    deriving Show
+
+import Data.Char (intToDigit)
+import Data.Foldable (find)
+import Data.Map (Map (..))
+import qualified Data.Map as Map
+import Data.Semigroup (Endo (..), stimesMonoid)
+import Data.Set (Set (..))
+import qualified Data.Set as Set
+import Week4.Exercise3
+import Week4.Exercise4
+
 
 isSimple :: Expr -> Bool
 isSimple exp= case exp of 
@@ -21,7 +27,7 @@ breadth Zero= 1
 breadth One= 1
 breadth (Add exp1 exp2)=1 + breadth exp1 + breadth exp2
 breadth (Mul exp1 exp2)=1 + breadth exp1 + breadth exp2
-breadth (Let s exp1 exp2)=2 + breadth exp1 + breadth exp2 
+breadth (Let s exp1 exp2)=1 + breadth exp1 + breadth exp2 
 breadth (Var _)= 1
 
 comp a b = if a>b then
@@ -38,3 +44,5 @@ depth (Let _ a b) = comp (depth a) (depth b)
 depth (Var _ )= 1
 
 
+commAdd::  Expr -> Expr
+commAdd exp=undefined
