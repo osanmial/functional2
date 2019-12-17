b4a27d8f217278ab54fe999d174c2a8530cce7b5
NaelSk <nael.soukouti@gmail.com>
2019-12-17 11:46:10 +0200

    CommAdd looks better now

--- src/Week7/Exercise3.hs
+++ src/Week7/Exercise3.hs
@@ -79,16 +79,16 @@ codistAddMul x = x
 
 
 commAdd::  Expr -> Expr
---commAdd (Let s exp1 exp2)
---   |(commAdd exp1)> (commAdd exp2)= (Let s (commAdd exp2) (commAdd exp1))
---   |otherwise= (Let s  (commAdd exp1) (commAdd exp2))
---commAdd (Add exp1 exp2)
---  | exp1 > exp2 = Add exp2 exp1 
---  |otherwise= Add exp1 exp2
+commAdd (Let s x y)= case (commAdd x, commAdd y) of
+  (z, w) | z > w -> Let s w z
+  (z, w) -> Let s z w
 commAdd (Add x y) = case (commAdd x, commAdd y) of
   (z, w) | z > w -> Add w z
   (z, w) -> Add z w
-commAdd (Mul x y) = Mul (commAdd x) (commAdd y)
+commAdd (Mul x y) = case  (commAdd x, commAdd y) of
+  (z, w) | z > w -> Mul w z
+  (z, w) -> Mul  z w
+
 
 closedStringBad :: String
 closedStringBad = "let \
