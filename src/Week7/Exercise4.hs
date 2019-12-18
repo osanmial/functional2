f979664222ab48d998a8ec236d18bcbc3e22f768
esko <esko@pop-os.localdomain>
2019-12-18 16:58:17 +0200

    Unbroken exercise 4

--- src/Week7/Exercise4.hs
+++ src/Week7/Exercise4.hs
@@ -5,8 +5,21 @@
 {-# OPTIONS_GHC -fwarn-incomplete-patterns  #-}
 {-# OPTIONS_GHC -fwarn-hi-shadowing #-}
 {-# OPTIONS_GHC -fwarn-name-shadowing #-}
+{-# LANGUAGE DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
+{-# LANGUAGE FlexibleInstances, TypeSynonymInstances #-}
 
 module Week7.Exercise4 where
+
+import Control.Arrow
+import Data.Function (on)
+import Data.Map (Map (..))
+import qualified Data.Map as Map
+import Data.Semigroup (Endo (..), stimesMonoid)
+import Data.Set (Set (..))
+import qualified Data.Set as Set
+import Week4.Exercise3
+import Week7.Exercise2
+import Week7.Exercise3
 
 import Data.Char (intToDigit)
 import Data.Foldable (find)
@@ -17,8 +30,8 @@ import Data.Set (Set (..))
 import qualified Data.Set as Set
 --import Week4.Exercise3
 --import Week4.Exercise4
-import Data.Range.Algebra (Algebra)
-import Data.Fix
+-- import Data.Range.Algebra (Algebra)
+-- import Data.Fix
 -- import Data.Fix
 
 {-
@@ -33,25 +46,31 @@ comonad reader field guide
 -- F-Algebra
 -- type Algebra f a = f a -> a 
 
-type  Expr'   = (Fix (Expr'')) 
-data Expr'' r = Add r r | Zero | Mul r r | One |
-   Let String r r | Var String deriving (Functor)
+-- type  Expr'   = (Fix (Expr'')) 
+-- data Expr'' r = Add r r | Zero | Mul r r | One |
+--    Let String r r | Var String deriving (Functor)
 --  $(deriveShow1 ''Expr'')
+
+-- pattern (Fix (Add l r)) = Add l r
+-- pattern (Fix Zero) = Zero  
+-- pattern (Fix (Mul l r )) = Mul l r
+-- pattern (Fix (One)) = One  
+-- pattern (Fix (Let s l r)) = Let s l r
+-- pattern (Fix (Var s)) = Var s
 
 isSimple :: Expr' -> Bool
 isSimple exp = cata isSimpleAlg exp
 
-
-isSimpleAlg :: Algebra Expr'' Bool
+isSimpleAlg :: Algebra ExprF Bool
 isSimpleAlg expr = case expr of
-  Let _ _ _ -> False
-  Var _ -> False
-  Add False _ -> False
-  Add _ False -> False
-  Mul False _ -> False
-  Mul _ False -> False
-  Zero -> True
-  One -> True
+  LetF _ _ _ -> False
+  VarF _ -> False
+  AddF False _ -> False
+  AddF _ False -> False
+  MulF False _ -> False
+  MulF _ False -> False
+  ZeroF -> True
+  OneF -> True
 
 {-case exp of
     Let _ _ _ -> False
@@ -61,17 +80,19 @@ isSimpleAlg expr = case expr of
 -}
   
 breadth :: Expr' -> Int
-breadth exp = cata breath' exp
+breadth exp = cata breadth' exp
 
-breadth' :: Algebra Expr'' Int
-breadth' Zero = 1
-breadth' One = 1
-breadth' (Add l r) = 1 + l + r
-breadth' (Mul l r) = 1 + l + r
-breadth' (Let s l r) = 1 + l + r
-breadth' (Var _) = 1
+breadth' :: Algebra ExprF Int
+breadth' ZeroF = 1
+breadth' OneF = 1
+breadth' (AddF l r) = 1 + l + r
+breadth' (MulF l r) = 1 + l + r
+breadth' (LetF s l r) = 1 + l + r
+breadth' (VarF _) = 1
 
--- assocAdd :: Expr' -> Expr'
+assocAdd :: Expr' -> Expr'
+assocAdd exp = undefined
+
 -- assocAdd (Add (Add a b) z) = assocAdd (Add a (Add b z))
 -- assocAdd (Add a b) = Add (assocAdd a) (assocAdd b)
 -- assocAdd (Mul a b) = Mul (assocAdd a) (assocAdd b)
@@ -122,141 +143,165 @@ breadth' (Var _) = 1
 --   Let String Expr' Expr' | Var String
 
 
-commAdd::  Expr' -> Expr'
+-- commAdd::  Expr' -> Expr'
 --commAdd (Let s exp1 exp2)
 --   |(commAdd exp1)> (commAdd exp2)= (Let s (commAdd exp2) (commAdd exp1))
 --   |otherwise= (Let s  (commAdd exp1) (commAdd exp2))
 --commAdd (Add exp1 exp2)
 --  | exp1 > exp2 = Add exp2 exp1 
 --  |otherwise= Add exp1 exp2
-commAdd (Add x y) = case (commAdd x, commAdd y) of
-  (z, w) | z > w -> Add w z
-  (z, w) -> Add z w
-commAdd (Mul x y) = Mul (commAdd x) (commAdd y)
+-- commAdd (Add x y) = case (commAdd x, commAdd y) of
+--   (z, w) | z > w -> Add w z
+--   (z, w) -> Add z w
+-- commAdd (Mul x y) = Mul (commAdd x) (commAdd y)
 
-closedStringBad :: String
-closedStringBad = "let \
-   \ two = 1 + 1 in let \
-   \ three = 1 * (1 + two * 1) in let \
-   \ five = let \
-     \ four = two * two in \
-     \ 1 + four in let \
-   \ six = let \
-     \ seven = 1 + six in \
-     \ two * three in let \
-   \ eight = two * four in let \
-   \ nine = 0 + three * (three + 0) in \
-   \ (0 * five + 1) + (three * (1 + nine) + eight * 0)"
-  
---closedDeepBad :: Expr'
---closedDeepBad = case parseExpr' closedStringBad of
---  Left e -> error (show e)
---  Right x -> x
 
--- | An improved version of the `showIntAtBase` function
--- from the `Numeric` module of the `base` package.
-showIntAtBase' :: forall a. Integral a => a -> (Int -> Char) -> a -> ShowS
-showIntAtBase' n f = let
-  g :: a -> ShowS
-  g p = case p `divMod` n of
-    (0, 0) -> id
-    (0, m) -> showChar (f (fromEnum m))
-    (d, m) -> g d . showChar (f (fromEnum m)) in
-  g
 
--- | A dual of the `keysSet` function
--- from the `Data.Map` module of the `containers` package.
-elemsSet :: Ord a => Map k a -> Set a
-elemsSet xs = Set.fromList (Map.elems xs)
+-- | Free-monadic version of the `(|||)` function
+-- from the `Control.Arrow` module of the `base` package.
+(||||) :: (a -> b) -> (m (Free' m a) -> b) -> Free' m a -> b
+f |||| g = let
+  h (Pure' x) = f x
+  h (Free' xs) = g xs in
+  h
+infixr 2 ||||
 
--- | A copy of the `thenCmp` function
--- from the `Language.Haskell.TH.Syntax` module
--- of the `template-haskell` package.
-thenCmp :: Ordering -> Ordering -> Ordering
-thenCmp EQ y = y
-thenCmp x _ = x
+-- | Cofree-comonadic version of the `(&&&)` function
+-- from the `Control.Arrow` module of the `base` package.
+(&&&&) :: (a -> b) -> (a -> m (Cofree' m b)) -> a -> Cofree' m b
+f &&&& g = let
+  h x = Cofree' (f x) (g x) in
+  h
+infixr 3 &&&&
 
-namePrefixes :: [String]
-namePrefixes = fmap pure ['x' .. 'z']
+type Algebra m a = m a -> a
 
-freshName :: Set String -> String
-freshName css = case find (`Set.notMember` css)
-  [(showString ds . showIntAtBase' 10 intToDigit n) mempty |
-    n <- [0 :: Int ..], ds <- namePrefixes] of
-  -- There are `on (*) toInteger maxBound (length namePrefixes)`
-  -- possible names and they are generated in a sorted order
-  -- to keep the behavior of the renamer predictable.
-  Nothing -> error "Out of names"
-  Just cs -> cs
+cata :: Functor m => Algebra m a -> Fix m -> a
+cata a = let
+  c = cata a in
+  a . fmap c . unFix
 
-findFree :: Expr' -> Set String
-findFree = let
-  f :: Expr' -> Set String
-  f (Add x y) = f x <> f y
-  f Zero = mempty
-  f (Mul x y) = f x <> f y
-  f One = mempty
-  f (Let cs x y) = f x <> Set.delete cs (f y)
-  f (Var cs) = Set.singleton cs in
+-- | Algebraic version of the `Endo` type
+-- from the `Data.Monoid` module of the `base` package.
+newtype Embed m = Embed {appEmbed :: Algebra m (Fix m)}
+
+instance Semigroup (Embed m) where
+  Embed g <> Embed f = Embed (g . unFix . f)
+
+instance Monoid (Embed m) where
+  mempty = Embed Fix
+
+type Coalgebra m a = a -> m a
+
+ana :: Functor m => Coalgebra m a -> a -> Fix m
+ana c = let
+  a = ana c in
+  Fix . fmap a . c
+
+hylo :: Functor m => Algebra m b -> Coalgebra m a -> a -> b
+-- Factored version of `hylo a c = cata a . ana c`.
+hylo a c = let
+  h = hylo a c in
+  a . fmap h . c
+
+-- | Coalgebraic version of the `Endo` type
+-- from the `Data.Monoid` module of the `base` package.
+newtype Project m = Project {appProject :: Coalgebra m (Fix m)}
+
+instance Semigroup (Project m) where
+  Project g <> Project f = Project (g . Fix . f)
+
+instance Monoid (Project m) where
+  mempty = Project unFix
+
+type ProductAlgebra m a = m (Fix m, a) -> a
+
+para :: Functor m => ProductAlgebra m a -> Fix m -> a
+para a = let
+  p = id &&& para a in
+  a . fmap p . unFix
+
+type SumCoalgebra m a = a -> m (Either (Fix m) a)
+
+apo :: Functor m => SumCoalgebra m a -> a -> Fix m
+apo c = let
+  a = id ||| apo c in
+  Fix . fmap a . c
+
+hypo :: Functor m => ProductAlgebra m b -> SumCoalgebra m a -> a -> b
+hypo a c = para a . apo c
+
+type CofreeAlgebra m a = m (Cofree' m a) -> a
+
+histo :: Functor m => CofreeAlgebra m a -> Fix m -> a
+histo a = let
+  h = histo a &&&& fmap h . unFix in
+  a . fmap h . unFix
+
+type FreeCoalgebra m a = a -> m (Free' m a)
+
+futu :: Functor m => FreeCoalgebra m a -> a -> Fix m
+futu c = let
+  f = futu c |||| Fix . fmap f in
+  Fix . fmap f . c
+
+chrono :: Functor m => CofreeAlgebra m b -> FreeCoalgebra m a -> a -> b
+chrono a c = histo a . futu c
+
+-- While we could reimplement all the old functions we have for `Expr`
+-- to obtain new equivalent functions for `Expr'`,
+-- that would be tedious and, in part,
+-- vain due to our poor choice of representation (not de Bruijn).
+-- Thus, we opt to merely derive an isomorphism and
+-- transport the old functions along it.
+
+fixExprCoalg' :: Coalgebra ExprF Expr
+fixExprCoalg' = let
+  f :: Expr -> ExprF Expr
+  f (Add x y) = AddF x y
+  f Zero = ZeroF
+  f (Mul x y) = MulF x y
+  f One = OneF
+  f (Let cs x y) = LetF cs x y
+  f (Var cs) = VarF cs in
   f
 
-compareExpr' :: Expr' -> Expr' -> Ordering
-compareExpr' = let
-  f :: Map String String -> Map String String -> Expr' -> Expr' -> Ordering
-  f css dss (Add x y) (Add z w) = f css dss x z `thenCmp` f css dss y w
-  f _ _ Zero Zero = EQ
-  f css dss (Mul x y) (Mul z w) = f css dss x z `thenCmp` f css dss y w
-  f _ _ One One = EQ
-  f css dss (Let cs x y) (Let ds z w) = f css dss x z `thenCmp` let
-    es = freshName (findFree y <> findFree w) in
-    f (Map.insert cs es css) (Map.insert ds es dss) y w
-  f css dss (Var cs) (Var ds) = case (Map.lookup cs css, Map.lookup ds dss) of
-    (Nothing, Nothing) -> cs `compare` ds
-    -- We must not compare free variables to bound variables,
-    -- because their ordering may be changed by the renamer.
-    -- This is fine, because free variables are never renamed.
-    (Nothing, Just _) -> LT
-    (Just _, Nothing) -> GT
-    (Just es, Just fs) -> es `compare` fs
-  -- The remaining cases are chosen in such a way that
-  -- "more constant" terms come before "more varying" ones.
-  -- This kind of convention may seem arbitrary,
-  -- but it comes up frequently in functional programming.
-  f _ _ Add {} Zero {} = GT
-  f _ _ Add {} Mul {} = LT
-  f _ _ Add {} One {} = GT
-  f _ _ Add {} Let {} = LT
-  f _ _ Add {} Var {} = LT
-  f _ _ Zero {} Add {} = LT
-  f _ _ Zero {} Mul {} = LT
-  f _ _ Zero {} One {} = LT
-  f _ _ Zero {} Let {} = LT
-  f _ _ Zero {} Var {} = LT
-  f _ _ Mul {} Add {} = GT
-  f _ _ Mul {} Zero {} = GT
-  f _ _ Mul {} One {} = GT
-  f _ _ Mul {} Let {} = LT
-  f _ _ Mul {} Var {} = LT
-  f _ _ One {} Add {} = LT
-  f _ _ One {} Zero {} = GT
-  f _ _ One {} Mul {} = LT
-  f _ _ One {} Let {} = LT
-  f _ _ One {} Var {} = LT
-  f _ _ Let {} Add {} = GT
-  f _ _ Let {} Zero {} = GT
-  f _ _ Let {} Mul {} = GT
-  f _ _ Let {} One {} = GT
-  f _ _ Let {} Var {} = LT
-  f _ _ Var {} Add {} = GT
-  f _ _ Var {} Zero {} = GT
-  f _ _ Var {} Mul {} = GT
-  f _ _ Var {} One {} = GT
-  f _ _ Var {} Let {} = GT in
-  f mempty mempty
+fixExpr' :: Expr -> Expr'
+fixExpr' = ana fixExprCoalg'
+
+unFixExprAlg' :: Algebra ExprF Expr
+unFixExprAlg' = let
+  f :: ExprF Expr -> Expr
+  f (AddF x y) = Add x y
+  f ZeroF = Zero
+  f (MulF x y) = Mul x y
+  f OneF = One
+  f (LetF cs x y) = Let cs x y
+  f (VarF cs) = Var cs in
+  f
+
+unFixExpr' :: Expr' -> Expr
+unFixExpr' = cata unFixExprAlg'
+
+-- showsPrecExpr' :: Int -> Expr' -> ShowS
+-- showsPrecExpr' n = showsPrecExpr n . unFixExpr'
+
+-- showsExpr' :: Expr' -> ShowS
+-- showsExpr' = showsPrecExpr' 0
+
+-- showExpr' :: Expr' -> String
+-- showExpr' = flip showsExpr' mempty
+
+-- closedDeepBad' :: Expr'
+-- closedDeepBad' = fixExpr' closedDeepBad
+
+-- closedDeep' :: Expr'
+-- closedDeep' = fixExpr' closedDeep
 
 instance Eq Expr' where
-  x == y = compareExpr x y == EQ
+  (==) = on (==) unFixExpr'
 
 instance Ord Expr' where
-  compare = compareExpr
+  compare = on compare unFixExpr'
+
 
