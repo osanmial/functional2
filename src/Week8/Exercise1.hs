dc19ba6426d7933dfe6adb1a4a7f13a6daa24f77
esko <esko@pop-os.localdomain>
2019-12-26 03:38:41 +0200

    trial and error

--- src/Week8/Exercise1.hs
+++ src/Week8/Exercise1.hs
@@ -1,1 +1,109 @@
 module Week8.Exercise1 where
+
+----    Derivative of Maybe a.
+{-
+
+Maybe a = Just a | Nothing
+(a + 1)
+derivative:
+1
+
+-}
+
+----    Derivative of Join (,) a.
+{-
+
+Join a = {runJoin :: p a a }
+Join (,) a -> (a,a) 
+-> (a×a)
+-> a^2
+derivative:
+2a
+==> (Bool, a)
+-}
+
+----    Derivative of [] a.
+{-
+(D_a(a (μx(a x +1)) + 1)
+-> (μz2 (D_a(a (μx(a x +1)) + 1) + (D_a(a (μx(a x +1)) + 1)) × z2))
+
+List a = a List a | []
+[a] -> μx (a x + 1)
+derivative
+D_x(μy (x y + 1))
+-> μz (λy(D_x(x y) + 1)(μy(x y + 1)) + λy(D_y(x y + 1))(μy(x y + 1)) × z)
+-> μz (λy(y)(μy(x y + 1)) + λy(x(μy(x y + 1))) × z)
+-> μz (μy(x y + 1) + x × z)
+-> μz (μy(x y + 1) + x × z)
+-> μy(x y + 1) + x × μz(μy(x y + 1) + x × z)
+-> (x μy(x y + 1) + 1) + x × μz(μy(x y + 1) + x × z)
+-> (x μy(x y + 1) + 1) + x × (μy(x y + 1) + x × μz(μy(x y + 1) + x × z))
+-> 
+
+-> (x μy(x y + 1) + 1) + x × (x × μy(x y + 1) + 1 + x × μz(μy(x y + 1) + x × z))
+
+-> (x μy(x y + 1) + 1) + x + x^2 × (μy(x y + 1) + μz(μy(x y + 1) + x × z)))
+-> x ×   (μy(x y + 1) + 1 + 1 + x × (μy(x y + 1) + μz(μy(x y + 1) + x × z))))
+-> x ×   (2 + μy(x y + 1) + x × (μy(x y + 1) + μz(μy(x y + 1) + x × z))))
+
+
+-> μy(x y + 1) × μy(x y + 1)
+-> 
+
+-> μz (D_x(x (μy(x y +1)) + 1) + (0 + 0 + D_y(μy(x y + 1) + 0) + 0) × z)
+-> μz (D_x(x (μy(x y +1)) + 1) + (0 + 0 + 1 + 0) + 0) × z)
+-> μz (D_x(x (μy(x y +1)) + 1) + z)
+
+-> 1 + D_x(μy(x y +1)) + μz (1 + D_x(μy(x y +1)) + z)
+
+
+-> μz (D_x(μy(x y +1)) + (D_x(μy(x y +1))) × z)
+-> (D_x(μy(x y +1)) + (D_x(μy(x y +1))) × (μz (D_x(μy(x y +1)) + (D_x(μy(x y +1))) × z)))
+
+...
+-> 
+-> μx (a x + 1) × μx (a x + 1)
+-> μx (a x + 1) × μx (a x + 1)
+-> [a]×[a]
+
+
+μy(x y + 1) sarja: 1+x+x^2+x^3+... vs 1+x(1 + x (1 + x (1 + x (...))) 
+
+
+Based on the ability of μy(x y + 1) being represented as a geometric series (1/(1-x))
+we can see that its derivative should be (1/(1-x))^2. But we don't even have a division operation defined for μy(x y + 1) so how does this come? beyond that there are far too many symbols in the paper I don't have the slightest clue about.
+
+the answer is apparently said to be :
+[a]×[a]
+-}
+
+----    Derivative of Stream a from the streams package.
+{-
+data Stream a = a :> Stream a
+μx (a,x)
+a×(a×(a×...))
+infinity
+
+the answer is probably something like :
+[a]×Stream a
+-}
+
+----    Derivative of Tree a from the binary-tree package.
+{-
+
+btree = 1 + btree^2
+btr
+D btree ->  2 × btree
+
+answer: (Bool, btree)
+-}
+
+
+----    Derivative of Tree a from the containers package.
+{-
+
+btree = 1 + ttree^3
+D btree ->  3 × ttree^2
+answer: ((Bool|()), (ttree,ttree))
+
+-}
--- src/Week8/Exercise2.hs
+++ src/Week8/Exercise2.hs
@@ -1,1 +1,135 @@
+{-# OPTIONS_GHC -XDeriveDataTypeable  #-}
+{-# OPTIONS_GHC -fwarn-incomplete-patterns  #-}
+{-# OPTIONS_GHC -fwarn-hi-shadowing #-}
+{-# OPTIONS_GHC -fwarn-name-shadowing #-}
+
 module Week8.Exercise2 where
+
+import Control.Exception
+import Control.Monad.Catch
+import Data.Stream.Infinite (Stream (..))
+import qualified Data.Tree as Rose (Forest (..), Tree (..))
+import qualified Data.Tree.Binary.Inorder as Binary (Tree (..))
+import Week8.Exercise1
+import Data.Bifunctor.Join
+import Data.Typeable
+
+data ZipperEnd = ZippersPrematureDeadEnd
+     deriving (Show, Typeable)
+
+instance Exception ZipperEnd
+
+--type A := Just a | Nothing
+-- -> a + 1 
+--context from derivative := 1
+--type ZA := (a, ())
+
+stepperMaybe :: MonadThrow m => Maybe a -> m (a, ())
+stepperMaybe (Just a) = pure (a,())
+stepperMaybe (Nothing) = throwM ZippersPrematureDeadEnd
+
+stepMaybe :: MonadThrow m => (a, ()) -> m (a, ())
+stepMaybe a = pure a
+
+--type A = Join (,) a
+{-
+
+Join a = {runJoin :: p a a }
+Join (,) a -> (a,a) 
+type (a×a)
+-> a^2
+context:
+2a
+==> (Bool, a)
+
+type ZA (a, (Bool, a))
+
+-}
+
+stepperJoin :: MonadThrow m => Join (,) a -> m (a, (Bool, a))
+stepperJoin jaa = pure (fst (runJoin jaa), (True, snd $ runJoin jaa ))
+
+stepJoin :: MonadThrow m => (a, (Bool, a)) -> m (a, (Bool, a))
+stepJoin (right, (False, other)) =  pure (other, (True, right))
+stepJoin (left, (True, other)) =  pure (other, (False, left))
+
+{-
+A := [a]
+context := [a]×[a]
+ZA := (a,[a]×[a])
+-}
+
+stepperList :: MonadThrow m => [a] -> m (a,([a], [a]))
+stepperList (x:xs) = pure (x, ([],xs))
+stepperList [] = throwM ZippersPrematureDeadEnd
+
+stepList :: MonadThrow m => (a,([a], [a])) -> m (a,([a],[a]))
+stepList (a,(as, b:bs)) = pure (b,(a:as,bs))
+stepList (_,(_, [])) = throwM ZippersPrematureDeadEnd
+
+stepperStream :: MonadThrow m => Stream a -> m (a, ([a],Stream a))
+stepperStream (a :> as) = pure (a, ([],as))
+
+stepStream :: MonadThrow m => (a, ([a],Stream a)) -> m (a, ([a],Stream a))
+stepStream (a, (as, b :> bs))= pure (b, (a:as, bs))
+
+{-
+list?
+μy(1 + y × a)
+(1 + y × a)
+D >> D(1 + y × a)
+y
+
+
+helper definition for list:
+  [a]
+≅ (μy(1 + x × y))
+≅ (1 + x × (μy(1 + x × y)))
+≅ (1 + x × (1 + x × (μy(1 + x × y))))
+≅ (1 + x × (1 + x × (1 + x × (μy(1 + x × y)))))
+≅ 1 + x × (1 + x × (1 + x × ...))
+≅ 1 + x + x^2 + x^3 × ...
+
+F := (1 + y × x)
+  D_x (μy(1 + y × x))
+≅ μz( (λy(D_x(1 + y × x))(μy.F)  +  (λy(D_y(1 + y × x))(μy.F)  × z)
+≅ μz( (λy(y)(μy.F)  +  (λy(x)(μy.F)  × z)
+≅ μz( (μy (1 + y × x))  +  x × z)
+≅ μz( (μy (1 + y × x))  +  x × z)
+≅ (μy (1 + y × x))  +  x × μz((μy (1 + y × x))  +  x × z)
+≅ (μy (1 + y × x))  +  x × ((μy (1 + y × x))  +  x × μz((μy (1 + y × x))  +  x × z))
+≅ [x] + (x×[x] + (x×[x] + (x×[x] + ... ))
+≅ [x] + [x] × (x + x + x + ... ))
+≅ [x] + [x] × x × (1 + 1 + 1 + ... ))
+≅ (1 + x + x^2 + x^3 × ...) + (1 + x + x^2 × x^3 + ...) × x × (1+1+1...)
+≅ (1 + x + x^2 + x^3 × ...) + (1 + x + x^2 × x^3 + ...) × (x+x+x...)
+≅ (1 + x + x^2 + x^3 × ...) + (1 + x + x^2 × x^3 + ...) × ( x + x^2 + x^3 ...)
+≅ (1 + x + x^2 × x^3 + ...) × (1 + x + x^2 + x^3 + ...)
+≅ [x] × [x]
+
+
+≅ (1 + x + x^2 + x^3 + ...) × (1 + x + x^2 + x^3 + ...)
+≅ (1 + x + x^2 + x^3 + ...) + (1 + x + x^2 + x^3 + ...) × (x + x^2 + x^3 + ...)
+≅ (1 + x + x^2 + x^3 + ...) + (x + x^2 + x^3 + ...)  +
+            (1 + x + x^2 + x^3 + ...) × (x^2 + x^3 + ...)
+
+≅ (1 + x + x^2 + x^3 + ...) + (x + x^2 + x^3 + ...) + (x^2 + x^3 + ...)  +
+                                (1 + x + x^2 + x^3 + ...) × (x^2 + x^3 + ...)
+
+≅
+
+()
+D(μy.F) ↦ μz.(((D_x F) |y=μy.F) + ((D_y F) |y=μy.F) × z)
+D(F|y=S) ↦ D_x(F|y=S) + D_y(F|y=S) × D_x S
+
+
+A := Leaf | Node (Tree a) a (Tree a)
+μy (1 + (y×y×a))
+(1 + (ay^2))
+D >> y^2
+(1 + (y×y×a)) × (1 + (y×y×a))
+context 
+-}
+
+stepBTree :: MonadThrow m => Binary.Tree a -> m (a, ([a],Stream a))
+stpeBtree = undefined
